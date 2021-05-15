56e1bdae5a2a5278ac10184215439b7c3e156c29
Anderson Olliver <pla.olliver@gmail.com>
2021-05-15 12:34:27 -0400

    Prepara splash screen

--- lib/app/core/splash_screen_page.dart
+++ lib/app/core/splash_screen_page.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashScreenPage extends StatelessWidget {
+  SplashScreenPage() {
+    Future.delayed(
+      Duration(seconds: 4),
+      () => Modular.to.pushReplacementNamed('/login'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Carregando'),
+    return Material(
+      color: Color(0xFF193283),
+      child: Center(
+        child: Column(
+          children: [
+            SvgPicture.asset(
+              'assets/logo-azulejo.svg',
+              semanticsLabel: 'Logotipo azulejo',
+              height: MediaQuery.of(context).size.height / 3,
+            ),
+            SizedBox(
+              height: (MediaQuery.of(context).size.height / 3) * 1,
+            ),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              strokeWidth: 2,
+            ),
+            SizedBox(
+              height: 36,
+            ),
+            Text(
+              'Azulejo',
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
